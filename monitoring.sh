66378469da31bf6e82cc0c892aa31c359350e16d
Anand <you@example.com>
2026-07-09 18:43:24 -0500

    Linux mointriong script

---
+++ monitoring.sh
@@ -0,0 +1,13 @@
+# !/bin/bash/
+
+# Monitoring  script
+
+disk=$( df -h | awk 'NR==2 { print $4 }' )
+cpu=$( top -bnl | grep Cpu | awk '{ print $3 }')
+memory= $( free -h | awk NR==2 { print $4}')
+
+echo "Disk usage: $disk"
+echo "CUp usage: $cpu"
+echo "Memory usage: $memeory"
+
+
