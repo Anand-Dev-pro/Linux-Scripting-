c4100e8f35a252721c594ad3039cf1743e5e91d6
Anand <you@example.com>
2026-07-09 18:54:19 -0500

    Monitoring script updated

--- monitoring.sh
+++ monitoring.sh
@@ -3,8 +3,8 @@
 # Monitoring  script
 
 disk=$( df -h | awk 'NR==2 { print $4 }' )
-cpu=$( top -bnl | grep Cpu | awk '{ print $3 }')
-memory= $( free -h | awk NR==2 { print $4}')
+cpu=$( top -bn1 | grep Cpu | awk '{ print $3 }')
+memory= $( free -h | awk NR==2 { print $3}')
 
 echo "Disk usage: $disk"
 echo "CUp usage: $cpu"
