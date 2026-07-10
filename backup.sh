d005e19775b05265e07893dd1f3a89461ecb37db
Anand <you@example.com>
2026-07-09 20:27:57 -0500

    Backup script

---
+++ backup.sh
@@ -0,0 +1,37 @@
+# !/bin/bash/
+
+
+backup="/backup "
+source_dir="/home/ubuntu"
+
+
+success_fail () {
+ if [ $? -eq 0 ]
+ then 
+	 echo " Backup done "
+ else
+	 echo " Backup failed "
+ fi 
+
+}
+
+if [ ! -d "$backup" ]
+then 
+	echo " The $backup dir is not exits... creating... "
+	mkdir $backup
+
+else 
+	echo " The $backup dir is available"
+
+fi 
+
+        tar -czf "$backup/backup_$(date +%Y-%m-%d).tar.gz" "source_dir" 
+
+	success_fail
+
+
+
+
+
+
+
